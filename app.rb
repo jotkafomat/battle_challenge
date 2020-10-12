0fb61173d26a89403a97b1397488436cf48c8c79
jotkafomat <k-j@xl.wp.pl>
2020-10-12 16:57:32 +0100

    add cat photo

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
+set :session_secret, 'super secret'
+
 get '/' do
-  "Hello World!"
+  "Hello World! with shotgun"
 end
 
 get '/secret' do
   "If you are on this page, and reading this, you should not be"
+end
+
+get '/cat' do
+"<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
--- config.ru
+++
@@ -1,1 +0,0 @@
-set :session_secret
